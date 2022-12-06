ff1074160977b82de97fa1fe84bc2d5314485580
Rajbhaiya1 <104322683+Rajbhaiya1@users.noreply.github.com>
2022-12-06 18:09:53 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -26,7 +26,7 @@ from .modules import authorize, list, cancel_mirror, mirror_status, mirror_leech
                      delete, count, users_settings, search, rss, wayback, speedtest, anilist, imdb, bt_select, mediainfo, hash, \
                      scraper, pictures
 from datetime import datetime
-from bot.helper.ext_utils import telegraph_helper
+from bot.helper.ext_utils.telegraph_helper import create_page
 
 def progress_bar(percentage):
     p_used = config_dict['FINISHED_PROGRESS_STR']
